00721d1fb7ba0c2dbda16498fc26c7501c914b62
dacaiguoguo <dacaiguoguo@163.com>
2013-03-27 15:34:09 +0800

    Initial Commit

---
+++ MoCai/SYGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SYGAppDelegate.h
+//  MoCai
+//
+//  Created by YangBin on 13-3-27.
+//  Copyright (c) 2013年 dacaiguoguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MoCai/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MoCai
+//
+//  Created by YangBin on 13-3-27.
+//  Copyright (c) 2013年 dacaiguoguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SYGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SYGAppDelegate class]));
+    }
+}
